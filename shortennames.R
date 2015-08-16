cd1d332e3b300a8a71ddd9877d5810eba4cdc150
Alana Alexander <laninsky@gmail.com>
2015-08-16 00:15:59 -0500

    Update shortennames.R

--- shortennames.R
+++ shortennames.R
@@ -26,5 +26,8 @@ output[i,2] <- paste(substr(output[i,1],1,5),"_",i,sep="")
 output[i,3] <- paste("sed -i 's/",output[i,1],"/",output[i,2],"/' $f;",sep="")
 }
 
-write.table(output[,3],"output",quote=FALSE, row.names=FALSE,col.names=FALSE)
+script <- as.matrix(output[,3])
+script <- rbind("for f in `ls *.nex`; do",script,"done")
+
+write.table(script,"shortnames.sh",quote=FALSE, row.names=FALSE,col.names=FALSE)
 write.table(output[,1:2],"shorten_names.txt",quote=FALSE, row.names=FALSE,col.names=FALSE)
