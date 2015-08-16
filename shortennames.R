a975f0181525d012c6140eef8d169682ff748a89
Alana Alexander <laninsky@gmail.com>
2015-08-15 23:33:05 -0500

    Update shortennames.R

--- shortennames.R
+++ shortennames.R
@@ -23,9 +23,8 @@ output <- matrix(nrow=norows,ncol=3)
 for (i in 1:norows) {
 output[i,1] <- unlist(strsplit(names[i,1]," "))[1]
 output[i,2] <- paste(substr(output[i,1],1,5),"_",i,sep="")
-output[i,3] <- paste("sed -i 's/",output[i,1],"/",output[i,2],"/' $f",sep="")
+output[i,3] <- paste("sed -i 's/",output[i,1],"/",output[i,2],"/' $f;",sep="")
 }
 
 write.table(output[,3],"output",quote=FALSE, row.names=FALSE,col.names=FALSE)
 write.table(output[,1:2],"shorten_names.txt",quote=FALSE, row.names=FALSE,col.names=FALSE)
-
