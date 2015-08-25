3f297f09c39a8df991b654a46e4908d3a60f7610
Alana Alexander <laninsky@gmail.com>
2015-08-25 11:35:15 -0500

    Update shortennames.R

--- shortennames.R
+++ shortennames.R
@@ -18,16 +18,20 @@ names <- rbind(names,input[i])
 }
 
 norows <- length(names)
-output <- matrix(nrow=norows,ncol=3)
+output <- matrix(nrow=norows,ncol=4)
 
 for (i in 1:norows) {
 output[i,1] <- unlist(strsplit(names[i,1]," "))[1]
 output[i,2] <- paste(substr(output[i,1],1,5),"_",i,sep="")
 output[i,3] <- paste("sed -i 's/",output[i,1],"/",output[i,2],"/' $f;",sep="")
+output[i,4] <- paste("sed -i 's/",output[i,2],"/",output[i,1],"/' $f;",sep="")
 }
 
-script <- as.matrix(output[,3])
-script <- rbind("for f in `ls *.nex`; do",script,"done")
+script1 <- as.matrix(output[,3])
+script1 <- rbind("for f in `ls *.nex`; do",script,"done")
 
-write.table(script,"shortnames.sh",quote=FALSE, row.names=FALSE,col.names=FALSE)
-write.table(output[,1:2],"shorten_names.txt",quote=FALSE, row.names=FALSE,col.names=FALSE)
+script2 <- as.matrix(output[,4])
+script2 <- rbind("for f in `ls *.csv`; do",script,"done")
+
+write.table(script1,"shortnames.sh",quote=FALSE, row.names=FALSE,col.names=FALSE)
+write.table(script1,"lengthennames.sh",quote=FALSE, row.names=FALSE,col.names=FALSE)
